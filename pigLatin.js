dc5fd6d92539dc36520c9fc58f9f643e1aee8e23
Katie Devops <angel_9586@hotmail.com>
2021-10-22 08:52:24 -0500

    Initial commit

---
+++ pigLatin.js
@@ -0,0 +1,9 @@
+// Write your solution below:
+
+function translatePigLatin(str) {
+    return str
+      .replace(/^[aeiou]\w*/, "$&way")
+      .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+  }
+  
+  console.log(translatePigLatin("consonant"))
